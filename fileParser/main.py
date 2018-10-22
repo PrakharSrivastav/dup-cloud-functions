b82ad00df1e5596c3fce8bafdf3e8abdc86e1aa2
Prakhar Srivastav <srivprakhar@gmail.com>
2018-10-22 23:46:15 +0200

    Connection to BigQuery

--- fileParser/main.py
+++ fileParser/main.py
@@ -1,25 +1,23 @@
 from google.cloud import pubsub_v1
 from google.cloud import storage
-import json
+from google.cloud import bigquery
+
 from google.auth.exceptions import DefaultCredentialsError
 from google.api_core.exceptions import AlreadyExists
-import time
-import csv
-from io import StringIO
+import time, csv, json
 
 # Setup configurations
 try:
     with open("config.json") as f:
-        configurations = f.read()
-    config = json.loads(configurations, encoding="UTF-8")
+        config = json.loads(f.read(), encoding="UTF-8")
 except Exception as e:
     raise Exception("Invalid configuration setup. Please validate if config.json is setup properly")
 
 # Setup storage and pubsub clients
 try:
-    storageClient = storage.Client()
-    publisher = pubsub_v1.PublisherClient()
+    storage_client = storage.Client()
     subscriber = pubsub_v1.SubscriberClient()
+    bq_client = bigquery.Client()
 except DefaultCredentialsError as e:
     raise Exception("Invalid login credentials. Please make sure GOOGLE_APPLICATION_CREDENTIALS is setup")
 except Exception as e:
@@ -28,71 +26,100 @@ except Exception as e:
 
 def callback(message):
     try:
-        print(message.data)
-        read_file_contents(json.loads(message.data, encoding="UTF-8"))
         message.ack()
+        bucket_info = json.loads(message.data, encoding="UTF-8")
+        keys = read_file_contents(json.loads(message.data, encoding="UTF-8"))
+        load_dataset_bq(bucket_info, keys)
+
     except KeyError as e:
         raise Exception("Please check the data published to pub_sub. One of the keys referenced does not exist")
     except Exception as e:
-        print("Some error", e)
+        print("Some error", type(e) ,e )
 
 
+def load_dataset_bq(bucket_info, keys):
+    try:
+        print(bucket_info)
+        print(keys)
+        uri = 'gs://{}/{}'.format(bucket_info.get("bucket"), bucket_info.get("name"))
+
+        dataset_id = bucket_info.get("name").replace(".", "_")
+        dataset = bigquery.Dataset(bq_client.dataset(dataset_id))
+        bq_client.create_dataset(dataset)
+        print("dataset_id {}".format(dataset_id))
+        dataset_ref = bq_client.dataset(dataset_id)
+
+        print("uri is {}".format(uri))
+        job_config = bigquery.LoadJobConfig()
+        job_config.schema = [bigquery.SchemaField(items, "STRING") for items in keys]
+
+        job_config.skip_leading_rows = 1
+        job_config.source_format = bigquery.SourceFormat.CSV
+        job_config.field_delimiter = ";"
+
+
+        load_job = bq_client.load_table_from_uri(
+            uri,
+            dataset_ref.table(dataset_id),
+            job_config=job_config
+        )
+        print('Starting job {}'.format(load_job.job_id))
+        load_job.result()  # Waits for table load to complete.
+        print('Job finished.')
+        destination_table = bq_client.get_table(dataset_id)
+        print('Loaded {} rows.'.format(destination_table.num_rows))
+    except KeyError as ex:
+        print("BQ : ", ex)
+
+
+# Stream the contents of the csv file to pubsub for example
 def read_file_contents(bucket_info):
     try:
         print(bucket_info)
-        bucket = storageClient.get_bucket(bucket_info.get("bucket"))
+        bucket = storage_client.get_bucket(bucket_info.get("bucket"))
         blob = bucket.get_blob(bucket_info.get("name"))
-        if blob.content_type == "text/csv":
-            print(blob.download_as_string().splitlines())
-            f = StringIO(blob.download_as_string().decode().splitlines())
-            # reader = csv.reader(f)
-            print((f.read()))
-            # for row in reader:
-            #     print('\t'.join(row))
-            # if csv_file is not None:
-            # f = blob.download_as_string()
-            # print(f)
-            # reader = csv.reader(f.split('\n'), csv.excel,delimiter=';')
-            # for row in reader:
-            #     print(row)
+        if blob is not None and blob.content_type == "text/csv":
+            lines = blob.download_as_string().decode().split("\n")
+            reader = csv.DictReader(lines, delimiter=";")
+            for row in reader:
+                if row and row is not None:
+                    # print(row['meterpoint_id'])
+                    print()
+                    return row.keys()
         else:
             raise Exception("Invalid file type. We only support csv files")
     except Exception as e:
-        print("Error parsin the file", e)
+        print("Error parsing the file", e)
         print(e.args)
-
-
-# if blob.content_type() =
 
 
 def subscribe_to_source_topic():
     # Create the subscription
     try:
-        topic_path = subscriber.topic_path(
-            config.get("PROJECT_ID"),
-            config.get("SOURCE_TOPIC")
-        )
+        project_id = config.get("PROJECT_ID")
+        source_topic = config.get("SOURCE_TOPIC")
+        topic_subscriber = config.get("SOURCE_TOPIC_SUBSCRIBER")
+        if project_id and source_topic and topic_subscriber:
+            topic = subscriber.topic_path(project_id, source_topic)
+            path = subscriber.subscription_path(project_id, topic_subscriber)
+            subscriber.create_subscription(path, topic)
+        else:
+            raise Exception("Configuration properties not set")
 
-        subscription_path = subscriber.subscription_path(
-            config.get("PROJECT_ID"),
-            config.get("SOURCE_TOPIC_SUBSCRIBER")
-        )
-
-        subscriber.create_subscription(subscription_path, topic_path)
-    except KeyError as e:
+    except KeyError:
         print("Invalid configuration keys")
         raise Exception("Trying to access wrong key from configuration. Please check your configuration access")
-    except AlreadyExists as e:
-        print("You are trying to create an existing subscription. Please change the name for your subscriber")
-        # print('Subscription created: {}'.format(subscription))
+
     except KeyboardInterrupt as e:
         print("Manual Interruption")
 
-    subscriber.subscribe(subscription_path, callback=callback)
+    except AlreadyExists as e:
+        print("You are trying to create an existing subscription. Please change the name for your subscriber")
 
-    print('Listening for messages on {}'.format(subscription_path))
+    subscriber.subscribe(path, callback=callback)
+    print('Listening for messages on {}'.format(path))
+
     while True:
-        print("reading")
         time.sleep(10)
 
 
